c2a4168be729e94b8d6bb5c30b8afd78bf236853
Andriy Drozdyuk <andriy@drozdyuk.com>
2019-11-05 09:35:50 -0500

    Wrote experiment setup for transformer pretraining

---
+++ data.py
@@ -0,0 +1,35 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+## Setup input pipline
+def get_datasets():
+    """
+    Use [TFDS](https://www.tensorflow.org/datasets) to load the IMDB movie reviews dataset with labels for positive or negative sentiments.
+
+    This dataset contains 25000 training examples and 25000 test examples.
+    Note: To keep this example small and relatively fast, drop examples with some max length.
+    """
+    DATA_DIR = 'data'
+    BUFFER_SIZE = 20000
+    BATCH_SIZE = 64
+    MAX_LENGTH = 200
+
+    (train_examples, test_examples), info = tfds.load('imdb_reviews/subwords8k',  data_dir=DATA_DIR,
+    split=['train', 'test'],
+    with_info=True, as_supervised=True)
+
+    def filter_max_length(review, _sentiment):
+        return tf.size(review) <= MAX_LENGTH
+
+    train_dataset = train_examples.filter(filter_max_length)
+    # cache the dataset to memory to get a speedup while reading from it.
+    train_dataset = train_dataset.cache()
+    train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(
+        BATCH_SIZE, padded_shapes=train_examples.output_shapes)
+    train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+    test_dataset = test_examples.filter(filter_max_length)
+    test_dataset = test_dataset.padded_batch(
+        BATCH_SIZE, padded_shapes=train_examples.output_shapes)
+
+    return train_dataset, test_dataset, info
---
+++ experiment.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+import tensorflow.keras.callbacks as cb
+from model import *
+from data  import get_datasets
+from tensorflow.keras.callbacks import Callback
+
+def study(epochs):
+    positional_encoding = [True, False]
+
+    for p in positional_encoding:
+        experiment(epochs, use_positional_encoding=p, load_checkpoint=False)
+
+class ParameterMetrics(Callback):
+    def __init__(self, use_positional_encoding):
+        super().__init__()
+        self.use_positional_encoding = use_positional_encoding
+
+    def on_epoch_end(self, epoch, logs):
+        logs['use_positional_encoding'] = int(self.use_positional_encoding)
+
+def experiment(epochs, use_positional_encoding, load_checkpoint):
+    print(f"Experiment: Positional Encoding={use_positional_encoding}")
+
+    train_dataset, test_dataset, info = get_datasets()    
+    vocab_size = info.features['text'].encoder.vocab_size 
+    
+    transformer = create_model(load_checkpoint, vocab_size, use_positional_encoding)
+
+    param_metrics = ParameterMetrics(use_positional_encoding)
+    history = fit_data(epochs, transformer, train_dataset, test_dataset, param_metrics)
+
+    eval_loss, eval_acc = transformer.evaluate(test_dataset)
+    print(f'[Evaluate] Best Loss: {eval_loss}, Best Acc: {eval_acc}')
+
+def fit_data(epochs, model, train_dataset, test_dataset, param_metrics):
+
+    tb = cb.TensorBoard()
+    csv = cb.CSVLogger('results.csv')
+    early = cb.EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
+    save = cb.ModelCheckpoint(filepath="checkpoints/train",
+             monitor='val_accuracy',
+             save_best_only=True,
+             save_weights_only=True)
+
+    model_history = model.fit(train_dataset,
+        validation_data=test_dataset,  
+        validation_freq=1,
+        shuffle=False,
+        verbose=1,
+        callbacks=[param_metrics, save, early, tb, csv],
+        epochs=epochs)
+    return model_history
+
+def create_model(load_checkpoint, vocab_size, use_positional_encoding):
+    
+    num_layers = 4
+    d_model = 128
+    dff = 512
+    num_heads = 8
+    
+    dropout_rate = 0.1    
+
+    transformer = TransformerEncoderClassifier(num_layers, d_model,  num_heads,
+    dff, vocab_size, pe_input=vocab_size,  rate=dropout_rate,
+    use_positional_encoding=use_positional_encoding)
+
+
+    loss_function = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    # transformer.run_eagerly = True
+    transformer.compile(optimizer=tf.keras.optimizers.Adam(), 
+        loss=loss_function, metrics=['accuracy'])
+
+    if load_checkpoint:
+        checkpoint = tf.train.latest_checkpoint("./checkpoints")
+        if checkpoint is not None:
+            print("Loading previously trained model.")
+            transformer.load_weights(checkpoint)
+
+    return transformer
+
+
+if __name__ == '__main__':
+    study(epochs=5)
